eee3361f46d0b0b1d1c45bf0fe7a7c1f1d9accf9
a1cd <71281043+a1cd@users.noreply.github.com>
2021-11-15 12:43:20 -0500

    Added algorithms

--- src/main/java/BaseConverter.java
+++ src/main/java/BaseConverter.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,7 +22,9 @@ public class BaseConverter {
      * @return the num in decimal form
      */
     public int strToInt(String num, String fromBase) {
-        return strToInt(num, Integer.getInteger(fromBase));
+        System.out.println('"'+fromBase+'"');
+        System.out.println(Integer.parseInt(fromBase.strip()));
+        return strToInt(num, Integer.parseInt(fromBase));
     }
     /**
      * @param num the number from which to convert to a decimal value
@@ -33,12 +36,6 @@ public class BaseConverter {
         for (int i = 0; i < num.length(); i++) {
             int index=((i-num.length())*-1)-1;
             count += baseChars.indexOf(num.charAt(index)) * ( (int) Math.pow(fromBase,i) );
-            System.out.println(i);
-            System.out.println(index);
-            System.out.println(baseChars.indexOf(num.charAt(index)));
-            System.out.println(fromBase);
-            System.out.println(baseChars.indexOf(num.charAt(index)) * ( (int) Math.pow(fromBase,i) ));
-            System.out.println();
         }
         return count;
     }
@@ -48,8 +45,23 @@ public class BaseConverter {
      * @param toBase the base to convert the number to
      * @return the converted number
      */
-    public int intToStr(int num, int toBase) {
-        return -1;
+    public String intToStr(int num, String toBase) {
+        return intToStr(num, Integer.parseInt(toBase));
+    }
+    public String intToStr(int num, int toBase) {
+        int currentTotal = num;
+        String out = "";
+        int neededChars = 0;
+        //noinspection StatementWithEmptyBody
+        for (neededChars++; Math.pow(toBase,neededChars)>num;) {}
+        for (int cChar = neededChars; cChar >= 0; cChar--) {
+            for (int i = toBase; i*Math.pow(toBase,cChar) >= currentTotal; i--) {
+                currentTotal=-i*((int) Math.pow(toBase, cChar));
+                //noinspection StringConcatenationInLoop
+                out = out + baseChars.get(i);
+            }
+        }
+        return out;
     }
 
     /**
@@ -61,10 +73,24 @@ public class BaseConverter {
         Scanner scanner;
         // create
         try {
-            File file = new File("./src/main/resources/values10.dat");
-               scanner = new Scanner(file);
+            File fileIn = new File("./src/main/resources/values10.dat");
+            File fileOut = new File("./src/main/resources/converted10.dat");
+            scanner = new Scanner(fileIn);
+            PrintWriter printWriter = new PrintWriter(fileOut);
             while(scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                String currentLine = scanner.nextLine();
+                String[] currentArgs = currentLine.split("\t");
+                int fromBase = Integer.parseInt(currentArgs[1]);
+                int toBase = Integer.parseInt(currentArgs[2]);
+                if (fromBase>16 || fromBase<2) {
+                    System.out.println("invalid fromBase value");
+                    continue;
+                } else if (toBase > 16 || toBase < 2) {
+                    System.out.println("invalid toBase value");
+                    continue;
+                }
+                String converted = intToStr(strToInt(currentArgs[0], currentArgs[1]), currentArgs[2]);
+                printWriter.write(converted+'\t'+currentArgs[1]+'\t'+currentArgs[2]);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
